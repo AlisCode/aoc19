431dcea0b7055e709554933a6f63c5216305f819
Olivier Pinon <op@impero.com>
2019-12-02 18:06:12 +0100

    day2 WIP

--- src/day2.rs
+++ src/day2.rs
@@ -21,27 +21,21 @@ impl Program {
     // true if the program should continue, false if it should stop
     pub fn next(&mut self) -> bool {
         let opcode = self.data[self.pointer];
+        let idx_a = self.data[self.pointer + 1] as usize;
+        let idx_b = self.data[self.pointer + 2] as usize;
+        let idx_c = self.data[self.pointer + 3] as usize;
         let res = match opcode {
             1 => {
-                let val = self.data[self.pointer + 1] + self.data[self.pointer + 2];
-                let index_to = self.data[self.pointer + 3] as usize;
-                self.data[index_to] = val;
+                let val = self.data[idx_a] + self.data[idx_b];
+                self.data[idx_c] = val;
                 true
             }
             2 => {
-                let val = self.data[self.pointer + 1] * self.data[self.pointer + 2];
-                let index_to = self.data[self.pointer + 3] as usize;
-                self.data[index_to] = val;
+                let val = self.data[idx_a] * self.data[idx_b];
+                self.data[idx_c] = val;
                 true
             }
-            99 => {
-                println!("Opcode is 99");
-                false
-            }
-            x => {
-                println!("Something went wrong");
-                false
-            }
+            _ => false,
         };
         if res {
             self.pointer += 4;
@@ -60,15 +54,34 @@ fn part_one(input: &[i32]) -> i32 {
     program.data[0]
 }
 
+#[aoc(day2, part2)]
+/// Solves part one by applying the calc_mass computation
+fn part_two(input: &[i32]) -> i32 {
+    let mut program = Program::new(input.to_vec());
+    program.data[1] = 13;
+    program.data[2] = 2;
+    while program.next() {}
+    program.data[0]
+}
+
 #[cfg(test)]
 pub mod tests {
-    use super::part_one;
+    use super::{part_one, Program};
 
     #[test]
     fn day2_part_one() {
+        let mut program = Program::new(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]);
+        assert!(program.next());
+        assert_eq!(program.pointer, 4);
         assert_eq!(
-            part_one(&vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]),
-            3500
+            program.data,
+            vec![1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
         );
+        assert!(program.next());
+        assert_eq!(
+            program.data,
+            vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+        );
+        assert!(!program.next());
     }
 }
