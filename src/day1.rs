61cfefcb6f092f5a044f3b9cf491bd94f149acf9
Olivier Pinon <oliv.pinon@gmail.com>
2019-12-02 18:54:03 +0100

    cargo fmt

--- src/day1.rs
+++ src/day1.rs
@@ -4,40 +4,34 @@ fn generator_input(input: &str) -> Vec<i32> {
     input.lines().map(|a| a.parse::<i32>().unwrap()).collect()
 }
 
-/// Calculates the fuel necessary for a given mass 
+/// Calculates the fuel necessary for a given mass
 fn calc_mass(x: &i32) -> i32 {
     x / 3 - 2
 }
 
-/// Calculates the recursive fuel necessary for a given mass 
+/// Calculates the recursive fuel necessary for a given mass
 fn calc_total_mass(x: &i32) -> i32 {
     (0..)
-    .scan(*x, |xx, _| {
-        *xx = calc_mass(xx);
-        if *xx > 0 {
-            return Some(*xx);
-        }
-        None
-    })
-    .sum()
+        .scan(*x, |xx, _| {
+            *xx = calc_mass(xx);
+            if *xx > 0 {
+                return Some(*xx);
+            }
+            None
+        })
+        .sum()
 }
 
 #[aoc(day1, part1)]
 /// Solves part one by applying the calc_mass computation
 fn part_one(input: &[i32]) -> i32 {
-    input
-    .iter()
-    .map(calc_mass)
-    .sum()
+    input.iter().map(calc_mass).sum()
 }
 
 #[aoc(day1, part2)]
 /// Solves part two by applying the calc_total_mass computation
 fn part_two(input: &[i32]) -> i32 {
-    input
-    .iter()
-    .map(calc_total_mass)
-    .sum()
+    input.iter().map(calc_total_mass).sum()
 }
 
 #[cfg(test)]
@@ -52,7 +46,7 @@ pub mod tests {
         assert_eq!(calc_mass(&100756), 33583);
     }
 
-    #[test] 
+    #[test]
     fn day1_total_mass() {
         assert_eq!(calc_total_mass(&14), 2);
         assert_eq!(calc_total_mass(&1969), 966);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 use aoc_runner_derive::aoc_main;
 
-aoc_main!{ lib = aoc19 }
+aoc_main! { lib = aoc19 }
