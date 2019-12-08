68c10b7fc06c8da037426adbf5a66916e11c1b06
Olivier Pinon <oliv.pinon@gmail.com>
2019-12-08 15:13:47 +0100

    Fix test day8 part two

--- src/day8.rs
+++ src/day8.rs
@@ -92,7 +92,8 @@ pub mod tests {
        assert_eq!(solve_part_one(&[1,2,1,0,0,0,0,0,0,0,1,2], 3, 2), 2);
     }
 
+    #[test]
     fn day8_part_two() {
-        assert_eq!(solve_part_two(&[0,2,2,2,1,1,2,2,2,2,1,2,0,0,0,0], 3, 2), "\n *\n* ");
+        assert_eq!(solve_part_two(&[0,2,2,2,1,1,2,2,2,2,1,2,0,0,0,0], 2, 2), "\n *\n* \n");
     }
 }
